8c9f0674be86fa5ef3746263431cb3795cf3208c
Prabhat Ranjan <33496438+pranjan4891@users.noreply.github.com>
2025-10-22 23:04:43 +0530

    Fix category filter URLs: Use absolute URLs for 'All Categories' and 'All Subcategories' links to properly redirect without incorrect path segments

--- resources/views/web/listing.blade.php
+++ resources/views/web/listing.blade.php
@@ -86,7 +86,7 @@
                                 <div class="filter-options-widget">
                                     <ul>
                                         <li class="{{ !isset($category) ? 'active' : '' }}">
-                                            <a href="{{ route('web.listing') }}" class="no-ajax">
+                                            <a href="{{ url('/add-listing') }}" class="no-ajax">
                                                 <i></i>
                                                 <span>All Categories</span>
                                             </a>
@@ -112,7 +112,7 @@
                                 <div class="filter-options-widget">
                                     <ul id="subCategoryList">
                                         <li class="{{ !isset($subcategory) ? 'active' : '' }}">
-                                            <a href="{{ isset($category) ? route('web.listing', $category->slug) : route('web.listing') }}" class="no-ajax">
+                                            <a href="{{ isset($category) ? url('/add-listing/' . $category->slug) : url('/add-listing') }}" class="no-ajax">
                                                 <i></i>
                                                 <span>All {{ isset($category) ? $category->name : 'Categories' }}</span>
                                             </a>
